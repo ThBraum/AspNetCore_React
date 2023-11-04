6afe98e1a78f25b9ca6e4886e032413468aff6ca
Matheus Braum <matheus_braum1@hotmail.com>
2023-11-04 16:35:24 -0300

    Modal

--- front/src/components/AtividadeForm.js
+++ front/src/components/AtividadeForm.js
@@ -1,5 +1,7 @@
+import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import { toast } from "react-toastify";
 import AtividadeList from "./AtividadeList";
 
@@ -28,7 +30,10 @@ let atividadeInicial = {
 export default function AtividadeForm(props) {
   const [atividades, setAtividades] = useState(initialState);
   const [atividade, setAtividade] = useState(atividadeInicial);
-  const [modoEdicao, setModoEdicao] = useState(false);
+  const [adicionarModo, setAdicionarModo] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [confirmacaoExclusao, setConfirmacaoExclusao] = useState(false);
+  const [atividadeExclusao, setAtividadeExclusao] = useState(null); // Novo estado
 
   useEffect(() => {
     const atividadesStorage = localStorage.getItem("atividades");
@@ -47,12 +52,14 @@ export default function AtividadeForm(props) {
 
   function handleEdit(atividade) {
     setAtividade(atividade);
-    setModoEdicao(true);
+    setAdicionarModo(false);
+    setShowModal(true);
   }
 
   function handleCancelar() {
-    setModoEdicao(false);
+    setAdicionarModo(true);
     setAtividade(atividadeInicial);
+    setShowModal(false);
   }
 
   function handleSubmit(event) {
@@ -87,14 +94,19 @@ export default function AtividadeForm(props) {
     }
 
     setAtividade(atividadeInicial);
-    setModoEdicao(false);
+    setShowModal(false);
     event.target.reset();
     localStorage.setItem("atividades", JSON.stringify(atividades));
   }
 
   function handleDelete(id) {
+    setConfirmacaoExclusao(true);
+    setAtividadeExclusao(id);
+  }
+
+  function confirmarExclusao() {
     const novasAtividades = atividades.filter((atividade) => {
-      return atividade.id !== id;
+      return atividade.id !== atividadeExclusao;
     });
 
     setAtividades(novasAtividades);
@@ -102,68 +114,107 @@ export default function AtividadeForm(props) {
       autoClose: 3000,
     });
     localStorage.setItem("atividades", JSON.stringify(novasAtividades));
+
+    setConfirmacaoExclusao(false);
+    setAtividadeExclusao(null);
+  }
+
+  function cancelarExclusao() {
+    setConfirmacaoExclusao(false);
+    setAtividadeExclusao(null);
   }
 
   return (
     <>
-      <Form onSubmit={handleSubmit} className="container mt-3">
-        <div className="row">
-          <Form.Group controlId="titulo" className="col-md-6">
-            <Form.Label>Título</Form.Label>
-            <Form.Control
-              type="text"
-              name="titulo"
-              placeholder="Digite o título da atividade"
-              value={atividade.titulo}
-              onChange={(e) =>
-                setAtividade({ ...atividade, titulo: e.target.value })
-              }
-            />
-          </Form.Group>
-          <Form.Group controlId="prioridade" className="col-md-6">
-            <Form.Label>Prioridade</Form.Label>
-            <Form.Select
-              name="prioridade"
-              value={atividade.prioridade}
-              onChange={(e) =>
-                setAtividade({ ...atividade, prioridade: e.target.value })
-              }
-            >
-              <option value="Selecione">Selecione...</option>
-              <option value="1">Baixa</option>
-              <option value="2">Normal</option>
-              <option value="3">Alta</option>
-            </Form.Select>
-          </Form.Group>
-        </div>
-        <Form.Group controlId="descricao">
-          <Form.Label>Descrição</Form.Label>
-          <Form.Control
-            as="textarea"
-            name="descricao"
-            rows={2}
-            placeholder="Digite a descrição"
-            value={atividade.descricao}
-            onChange={(e) =>
-              setAtividade({ ...atividade, descricao: e.target.value })
-            }
-          />
-        </Form.Group>
-        <div style={{ display: "flex", marginTop: "1rem" }}>
-          <Button variant="primary" type="submit" className="col-md-3 me-2">
-            {modoEdicao ? "Atualizar" : "Adicionar"}
+      <div className="d-flex justify-content-between align-items-center mb-3 mt-3">
+        <h2>Atividades</h2>
+        <Button
+          variant="outline-secondary"
+          size="lg"
+          onClick={() => setShowModal(true)}
+        >
+          <FontAwesomeIcon icon={faSquarePlus} />
+        </Button>
+      </div>
+      <hr />
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              {adicionarModo ? "Adicionar Atividade" : "Editar Atividade"}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group controlId="titulo">
+              <Form.Label>Título</Form.Label>
+              <Form.Control
+                type="text"
+                name="titulo"
+                placeholder="Digite o título da atividade"
+                value={atividade.titulo}
+                onChange={(e) =>
+                  setAtividade({ ...atividade, titulo: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group controlId="prioridade">
+              <Form.Label>Prioridade</Form.Label>
+              <Form.Select
+                name="prioridade"
+                value={atividade.prioridade}
+                onChange={(e) =>
+                  setAtividade({ ...atividade, prioridade: e.target.value })
+                }
+              >
+                <option value="Selecione">Selecione...</option>
+                <option value="1">Baixa</option>
+                <option value="2">Normal</option>
+                <option value="3">Alta</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group controlId="descricao">
+              <Form.Label>Descrição</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="descricao"
+                rows={2}
+                placeholder="Digite a descrição"
+                value={atividade.descricao}
+                onChange={(e) =>
+                  setAtividade({ ...atividade, descricao: e.target.value })
+                }
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <div style={{ display: "flex", marginTop: "1rem" }}>
+              <Button variant="secondary" onClick={handleCancelar}>
+                Cancelar
+              </Button>
+              <Button variant="primary" type="submit">
+                {adicionarModo ? "Adicionar" : "Atualizar"}
+              </Button>
+            </div>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+      <Modal show={confirmacaoExclusao} onHide={cancelarExclusao}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmar Exclusão</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Tem certeza de que deseja excluir esta atividade?</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={cancelarExclusao}>
+            Não
           </Button>
-          {modoEdicao && (
-            <Button
-              variant="danger"
-              onClick={handleCancelar}
-              className="col-md-3"
-            >
-              Cancelar
-            </Button>
-          )}
-        </div>
-      </Form>
+          <Button variant="danger" onClick={confirmarExclusao}>
+            Sim
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <AtividadeList
         atividades={atividades}
         handleDelete={handleDelete}
